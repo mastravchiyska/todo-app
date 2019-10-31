81a7128e5bfddc462996efea3dfc0dc27be1e437
Mastravchiyska <tmas@softwareag.com>
2019-10-31 12:59:29 +0200

    Ädding task-list to app with mock data

--- client/todo-app/src/app/app.component.ts
+++ client/todo-app/src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'ToDo Task Manager';
+
+  tasks = [
+    {
+      "createdAt": 1572437538669,
+      "updatedAt": 1572437538669,
+      "id": 1,
+      "title": "Task 1",
+      "description": "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originallybred for hunting.The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originallybred for hunting.",
+      "priority": 0,
+      "completed": true,
+      "project": null
+    },
+    {
+      "createdAt": 1572437538669,
+      "updatedAt": 1572437538669,
+      "id": 1,
+      "title": "Task 1",
+      "description": "Add new task",
+      "priority": 0,
+      "completed": false,
+      "project": null
+    },
+    {
+      "createdAt": 1572437538669,
+      "updatedAt": 1572437538669,
+      "id": 1,
+      "title": "Task 1",
+      "description": "Add new task",
+      "priority": 0,
+      "completed": false,
+      "project": null
+    }
+  ]
+
+  openedTasks = this.tasks.filter(task => task.completed !==true);
+  completedTasks = this.tasks.filter(task => task.completed ===true);
 }
--- client/todo-app/src/app/app.module.ts
+++ client/todo-app/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { TasksModule } from './tasks/tasks.module'
+import { MatGridListModule } from '@angular/material/grid-list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NoopAnimationsModule,
+    TasksModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
