cbcfe3959ffcd7566fb3f01a74c0dad714b34743
Mastravchiyska <tmas@softwareag.com>
2019-11-02 18:51:02 +0200

    Fixes and refactor task-list

--- client/todo-app/src/app/tasks/task-list/task-list.component.ts
+++ client/todo-app/src/app/tasks/task-list/task-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TasksService } from 'src/app/services/tasks.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Task } from '../Task';
 
 @Component({
   selector: 'app-task-list',
@@ -8,11 +9,16 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
-  tasks;
-  isSelected = false;
-  selectedTask: object;
+
+  tasks: Task[] = [];
+  selectedTaskId: number;
+  mode: string;
 
   constructor(private http: TasksService, private _snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.updateList();
+  }
 
   showMessage(message: string, action: string) {
     this._snackBar.open(message, action, {
@@ -20,59 +26,53 @@ export class TaskListComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-    this.http.listAll().subscribe(
-      tasks => this.tasks = tasks,
-      err => console.log(err)
-    );
-  }
-
-  selectTask(task) {
-    this.isSelected = true;
-    this.selectedTask = task;
-    console.log(this.selectedTask)
-  }
-  createTask() {
-    this.isSelected = true;
-    this.selectTask({ "title": undefined });
+  selectTask(task, mode) {
+    if (mode == 'edit' && task) {
+      this.mode = mode;
+      this.selectedTaskId = task.id;
+    }
+    if (mode == 'new') {
+      this.mode = mode;
+    }
   }
 
   onCanceled() {
-    this.isSelected = false;
+    this.mode = '';
   }
 
-  updateTask(task) {
-    if (task.id) {
-      this.http.update(task).subscribe(
-        t => console.log('updated', t)
-      )
-    } else {
-      this.http.addTask(task).subscribe(
-        t => this.tasks.push(t))
-    }
-    
+  onSave() {
+    this.updateList();
+    this.onCanceled();
     this.showMessage('Task was saved successfully!', 'NEW');
   }
 
+  updateList() {
+    this.http.listAll().subscribe(
+      (tasks: Task[]) => this.tasks = tasks,
+      err => this.showMessage(err, 'ERROR')
+    )
+  }
+
   completeTask(task) {
-    this.selectedTask = task;
-    if (task.completed === false) {
-      this.selectedTask.completed = true;
-      this.http.update(task).subscribe(
-        t => console.log('completed', t)
-      )
+    if (task.completed) {
+      return false;
     }
-    this.showMessage('Task was completed!', 'COMPLETE')
+
+    task.completed = true;
+    this.http.update(task).subscribe(
+      t => this.showMessage('Task was completed!', 'COMPLETE'),
+      err => this.showMessage(err, 'ERROR')
+    );
   }
 
   deleteTask(task) {
     this.http.delete(task.id).subscribe(
       t => {
-        const index = this.tasks.findIndex(tsk => tsk.id === t.id);
-        this.tasks.splice(index, 1);
-      }
+        this.updateList();
+        this.showMessage('Task was deleted successfully!', 'DELETE');
+      },
+      err => this.showMessage(err, 'ERROR')
     )
-    this.showMessage('Task was deleted successfully!', 'DELETE')
   }
 
 }
